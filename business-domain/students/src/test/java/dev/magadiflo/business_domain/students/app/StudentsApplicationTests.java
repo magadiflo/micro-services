823dd94ec22b473ba81e36f0e202a9ca37faa767
Martín <magadiflo@gmail.com>
2023-12-19 11:50:36 -0500

    Creando micro servicio Students

---
+++ business-domain/students/src/test/java/dev/magadiflo/business_domain/students/app/StudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.magadiflo.business_domain.students.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
