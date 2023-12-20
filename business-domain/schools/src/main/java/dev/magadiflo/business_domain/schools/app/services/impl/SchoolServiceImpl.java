f0877c1dc97f3328002f21a4ef589b9e25837155
Martín <magadiflo@gmail.com>
2023-12-19 19:59:04 -0500

    Configurando API Gateway con Load Balanced

--- business-domain/schools/src/main/java/dev/magadiflo/business_domain/schools/app/services/impl/SchoolServiceImpl.java
+++ business-domain/schools/src/main/java/dev/magadiflo/business_domain/schools/app/services/impl/SchoolServiceImpl.java
@@ -5,7 +5,6 @@ import dev.magadiflo.business_domain.schools.app.models.dtos.Student;
 import dev.magadiflo.business_domain.schools.app.models.entities.School;
 import dev.magadiflo.business_domain.schools.app.repositories.ISchoolRepository;
 import dev.magadiflo.business_domain.schools.app.services.ISchoolService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,12 +13,16 @@ import org.springframework.web.client.RestClient;
 import java.util.List;
 import java.util.Optional;
 
-@RequiredArgsConstructor
 @Service
 public class SchoolServiceImpl implements ISchoolService {
 
     private final ISchoolRepository schoolRepository;
-    private final RestClient restClient = RestClient.create();
+    private final RestClient restClient;
+
+    public SchoolServiceImpl(ISchoolRepository schoolRepository, RestClient.Builder restClientBuilder) {
+        this.schoolRepository = schoolRepository;
+        this.restClient = restClientBuilder.baseUrl("lb://students/api/v1/students").build();
+    }
 
     @Override
     @Transactional(readOnly = true)
@@ -33,7 +36,7 @@ public class SchoolServiceImpl implements ISchoolService {
         return this.schoolRepository.findById(schoolId)
                 .map(schoolDB -> {
                     List<Student> students = this.restClient.get()
-                            .uri("http://localhost:8081/api/v1/students/school/{schoolId}", schoolDB.getId())
+                            .uri("/school/{schoolId}", schoolDB.getId())
                             .retrieve()
                             .body(new ParameterizedTypeReference<>() {
                             });
